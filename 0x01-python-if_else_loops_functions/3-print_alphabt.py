bcb169fa53c8422ad4dabb0466bf070535720498
yoyogold-a11 <tmrfack@gmail.com>
2021-04-27 20:09:15 +0000

    fixing code

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in range(97, 123):
+for a in range(ord('a'), ord('z') + 1):
     if chr(a) != 'e' and chr(a) != 'q':
-        print(chr(a), end='')
+        print("{:c}".format(a), end='')
