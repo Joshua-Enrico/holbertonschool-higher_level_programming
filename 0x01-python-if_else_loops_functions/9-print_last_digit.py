10d2b89d4535ffaa7fbec50921ecf53bbfacb8e9
yoyogold-a11 <tmrfack@gmail.com>
2021-04-28 02:00:58 +0000

    fixing code

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,4 +5,4 @@ def print_last_digit(number):
         return(-(number % -10))
     else:
         print("{:d}".format(number % 10), end='')
-        return(-(number % 10))
+        return((number % 10))
