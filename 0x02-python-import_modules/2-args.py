37cf7cc1e226ae3319c07e2487f9cc3daa150598
yoyogold-a11 <tmrfack@gmail.com>
2021-04-29 20:41:27 +0000

    fixing code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,10 @@ if __name__ == '__main__':
 if len(sys.argv) == 1:
     print(0, 'arguments:')
 else:
-    print(len(sys.argv) - 1, 'arguments:')
-    for a in range(1, len(sys.argv)):
-        print('{}:'.format(a), sys.argv[a])
+    if len(sys.argv) is 2:
+        print(len(sys.argv) - 1, 'arguments:')
+        print("{}:".format(len(sys.argv) - 1), sys.argv[1])
+    else:
+        print(len(sys.argv) - 1, 'arguments:')
+        for a in range(1, len(sys.argv)):
+            print('{}:'.format(a), sys.argv[a])
