3c6217fd7389ef2c102375226da128e944a41f3e
yoyogold-a11 <tmrfack@gmail.com>
2021-05-06 19:56:51 +0000

    add uniques integers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    add = 0
     new_list = set(my_list)
-    sumall = sum(new_list)
-    return(sumall)
+    for elm in new_list:
+        add += elm
+    return(add)
