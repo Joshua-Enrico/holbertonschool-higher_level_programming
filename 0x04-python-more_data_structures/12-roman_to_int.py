cc1bee1dcc167a82edb5ed8d74a8bd6852c2e8f1
yoyogold-a11 <tmrfack@gmail.com>
2021-05-07 02:02:54 +0000

    romans

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if roman_string:
+        letter = {'I': 1, 'V': 5, 'X': 10,
+                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        new_list = list(roman_string)
+        for elm in new_list:
+            for elm2 in letter:
+                if elm == elm2:
+                    if total < letter[elm2]:
+                        total = letter[elm2] - total
+                    else:
+                        total += letter[elm2]
+                    break
+        return total
+    else:
+        return 0
