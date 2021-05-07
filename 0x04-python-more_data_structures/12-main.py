54520ed7cf117443bacd67143367a24a57c3e84f
yoyogold-a11 <tmrfack@gmail.com>
2021-05-07 02:06:35 +0000

    romans

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -3,10 +3,10 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = ""
+roman_number = "X"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = None
+roman_number = "VII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "IX"
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string:
-        letter = {'I': 1, 'V': 5, 'X': 10,
-                  'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        new_list = list(roman_string)
-        for elm in new_list:
-            for elm2 in letter:
-                if elm == elm2:
-                    if total < letter[elm2]:
-                        total = letter[elm2] - total
-                    else:
-                        total += letter[elm2]
-                    break
-        return total
-    else:
+    if roman_string is None or isinstance(roman_string, str) is False:
         return 0
+    letter = {'I': 1, 'V': 5, 'X': 10,
+                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    new_list = list(roman_string)
+    for elm in new_list:
+        for elm2 in letter:
+            if elm == elm2:
+                if total < letter[elm2]:
+                    total = letter[elm2] - total
+                else:
+                    total += letter[elm2]
+                break
+    return total
