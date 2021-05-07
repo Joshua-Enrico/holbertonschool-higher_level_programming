fce9e3e681d4afef57b065e71d86073314a1b90f
yoyogold-a11 <tmrfack@gmail.com>
2021-05-07 02:52:10 +0000

    romans to decimal

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or isinstance(roman_string, str) is False:
+    if roman_string is None:
         return 0
     letter = {'I': 1, 'V': 5, 'X': 10,
-                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+              'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    new_list = list(roman_string)
-    for elm in new_list:
-        for elm2 in letter:
-            if elm == elm2:
-                if total < letter[elm2]:
-                    total = letter[elm2] - total
-                else:
-                    total += letter[elm2]
-                break
+    maxchar = 'I'
+    new_list = roman_string[::-1]
+    for elm2 in new_list:
+        if letter[elm2] >= letter[maxchar]:
+            maxchar = elm2
+            total += letter[elm2]
+        else:
+            total -= letter[elm2]
     return total
