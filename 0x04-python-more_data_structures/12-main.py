56fce58a9bc5a54ae2f1199d50e2509342f9c73c
yoyogold-a11 <tmrfack@gmail.com>
2021-05-07 02:58:33 +0000

    romans to deciaml

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -6,7 +6,7 @@ roman_to_int = __import__('12-roman_to_int').roman_to_int
 roman_number = "MMMCMXCIX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "VII"
+roman_number = None
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "IX"
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if roman_string is None or isinstance(roman_string, str) is False:
         return 0
     letter = {'I': 1, 'V': 5, 'X': 10,
               'L': 50, 'C': 100, 'D': 500, 'M': 1000}
