7ff68edda30898e70b5d1e828f50091592c3ae3e
yoyogold-a11 <tmrfack@gmail.com>
2021-05-17 11:24:26 +0000

    last ejer

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return False
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,8 @@
+def safe_function(fct, *args):
+    try:
+        div = fct(*args)
+        return div
+    except Exception as error:
+        import sys
+        print("Exception: {}".format(error), file=sys.stderr)
+        return None
