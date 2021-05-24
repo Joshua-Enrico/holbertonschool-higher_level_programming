cb08d9bb02622532eba16c3413fe08daddcc5856
yoyogold-a11 <tmrfack@gmail.com>
2021-05-24 10:35:47 +0000

    first commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Empty Class"""
+class Rectangle:
+    """Defines a Empty Rectangule Class"""
+    pass
---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""
+Defines a Rectangle Class
+"""
+
+
+class Rectangle:
+    """Represents a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """Defines  a Rectangle"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """setter of private instance width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter of private instance width attribute"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """setter of private instance height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter of private instance height attribute"""
+        if type(value) is not int:
+            raise TypeError("height must be integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of a square"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeters of a square"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return a square"""
+        string = ""
+        if self.__width == 0 and self.__height == 0:
+            return string
+        for i in range(self.__height):
+            for b in range(self.__width):
+                string += "#"
+            if i < self.__height - 1:
+                string += "\n"
+        return string
+
+    def __repr__(self):
+        """Represents a rectangle"""
+        return "Rectangle(" + str(self.__width) + ", " \
+                            + str(self.__height) + ")"
+
+    def __del__(self):
+        """deletion of instance"""
+        print("Bye rectangle...")
