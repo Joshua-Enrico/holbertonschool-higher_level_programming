9556a35ef72c5259f2c6b43163af3271cae6ab4b
yoyogold-a11 <tmrfack@gmail.com>
2021-05-24 23:45:50 +0000

    fixing code

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -77,16 +77,15 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-         """Compare squares"""
+        """ bigger or equal instance """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() > rect_2.area():
+        if rect_1.area() >= rect_2.area():
             return rect_1
-        if rect_1.area() < rect_2.area():
+        else:
             return rect_2
-        return rect_1
 
     @classmethod
     def square(cls, size=0):
