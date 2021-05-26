66e067003ac9f8e82dbf47f2d590fe29ea51a947
yoyogold-a11 <tmrfack@gmail.com>
2021-05-26 00:36:02 +0000

    first commit

---
+++ 0x09-python-everything_is_object/1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+a = 10
+b = 10
+
+print(a)
+print(b)
+
+if a == b:
+    print("true")
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+Copy a list
+"""
+def copy_list(l):
+    new_list = l.copy()
+    return new_list
