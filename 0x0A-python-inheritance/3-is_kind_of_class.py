d4817ae11e338b3a092ae9fb5fbdf4a25c37f0d9
yoyogold-a11 <tmrfack@gmail.com>
2021-06-01 23:15:40 +0000

    several adittions

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- returns True if the object is an instance of, 
- or if the object is an instance 
+ returns True if the object is an instance of,
+ or if the object is an instance
  of a class that inherited from, t
  he specified class ; otherwise False.
 """
