f2551b42a61f4e734dc1e40e1633588e12867ecf
yoyogold-a11 <tmrfack@gmail.com>
2021-06-03 03:00:39 +0000

    add files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """read a text file(utf9) and prints it"""
     with open(filename, "r", encoding=("utf8")) as text:
         lines = text.read()
-    print(lines)
+    print(lines, end='')
