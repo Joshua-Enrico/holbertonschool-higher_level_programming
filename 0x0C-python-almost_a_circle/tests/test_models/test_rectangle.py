302d814a7ced9a346a5b5e95b7a1319f67c885ac
yoyogold-a11 <tmrfack@gmail.com>
2021-06-08 16:44:26 -0500

    testing

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -474,69 +474,72 @@ class Test_Dictionary_Representation(unittest.TestCase):
 
         self.assertEqual(r1 == r2, False)
 
-    def test_load_from_file(self):
-        """test normal use of load_from_file"""
-        r1 = Rectangle(1, 2, 3, 4, 5)
-        r2 = Rectangle(6, 7, 8, 9, 10)
-        li = [r1, r2]
-        Rectangle.save_to_file(li)
-        lo = Rectangle.load_from_file()
-        self.assertTrue(type(lo) is list)
-        self.assertEqual(len(lo), 2)
-        r1c = lo[0]
-        r2c = lo[1]
-        self.assertTrue(type(r1c) is Rectangle)
-        self.assertTrue(type(r2c) is Rectangle)
-        self.assertEqual(str(r1), str(r1c))
-        self.assertEqual(str(r2), str(r2c))
-        self.assertIsNot(r1, r1c)
-        self.assertIsNot(r2, r2c)
-        self.assertNotEqual(r1, r1c)
-        self.assertNotEqual(r2, r2c)
+    def test_mandatory_width(self):
+        """Test that width is a mandatory arg"""
+        with self.assertRaises(TypeError):
+            r = Rectangle()
 
-class TestRectangle(unittest.TestCase):
-    """Test the functionality of the Rectangle class"""
-    @classmethod
-    def setUpClass(cls):
-        """"""
-        Base._Base__nb_objects = 0
-        cls.r1 = Rectangle(10, 10)
-        cls.r2 = Rectangle(2, 3, 4)
-        cls.r3 = Rectangle(5, 6, 7, 8, 9)
-        cls.r4 = Rectangle(11, 12, 13, 14)
+    def test_mandatory_height(self):
+        """Test that height is a mandatory arg"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(1)
 
-    def test_id(self):
-        """Test for functioning ID"""
-        self.assertEqual(self.r1.id, 1)
-        self.assertEqual(self.r2.id, 2)
-        self.assertEqual(self.r3.id, 9)
-        self.assertEqual(self.r4.id, 3)
+    def test_width_typeerror(self):
+        """Test non-ints for width"""
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            r = Rectangle("hello", 1)
+        with self.assertRaisesRegex(TypeError, "width must be an integer"):
+            r = Rectangle(True, 1)
 
-    def test_width(self):
-        """Test for functioning width"""
-        self.assertEqual(self.r1.width, 10)
-        self.assertEqual(self.r2.width, 2)
-        self.assertEqual(self.r3.width, 5)
-        self.assertEqual(self.r4.width, 11)
+    def test_height_typeerror(self):
+        """Test non-ints for height"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            r = Rectangle(1, "hello")
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
+            r = Rectangle(1, True)
 
-    def test_height(self):
-        """Test for functioning height"""
-        self.assertEqual(self.r1.height, 10)
-        self.assertEqual(self.r2.height, 3)
-        self.assertEqual(self.r3.height, 6)
-        self.assertEqual(self.r4.height, 12)
+    def test_x_typeerror(self):
+        """Test non-ints for x"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            r = Rectangle(1, 1, "hello")
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            r = Rectangle(1, 1, True)
 
-    def test_x(self):
-        """Test for functioning x"""
-        self.assertEqual(self.r1.x, 0)
-        self.assertEqual(self.r2.x, 4)
-        self.assertEqual(self.r3.x, 7)
-        self.assertEqual(self.r4.x, 13)
+    def test_y_typeerror(self):
+        """Test non-ints for y"""
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            r = Rectangle(1, 1, 1, "hello")
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            r = Rectangle(1, 1, 1, True)
 
-    def test_y(self):
-        """Test for functioning y"""
-        self.assertEqual(self.r1.y, 0)
-        self.assertEqual(self.r2.y, 0)
-        self.assertEqual(self.r3.y, 8)
-        self.assertEqual(self.r4.y, 14)
+    def test_width_valueerror(self):
+        """Test ints <= 0 for width"""
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            r = Rectangle(-1, 1)
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
+            r = Rectangle(0, 1)
+
+    def test_height_valueerror(self):
+        """Test ints <= 0 for height"""
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            r = Rectangle(1, -1)
+        with self.assertRaisesRegex(ValueError, "height must be > 0"):
+            r = Rectangle(1, 0)
+
+    def test_x_valueerror(self):
+        """Test ints < 0 for x"""
+        with self.assertRaisesRegex(ValueError, "x must be >= 0"):
+            r = Rectangle(1, 1, -1)
+
+    def test_y_valueerror(self):
+        """Test ints <= 0 for y"""
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
+            r = Rectangle(1, 1, 1, -1)
+
+    def test_area(self):
+        """test area"""
+        self.assertEqual(self.r1.area(), 100)
+        self.assertEqual(self.r2.area(), 6)
+        self.assertEqual(self.r3.area(), 30)
+        self.assertEqual(self.r4.area(), 132)
 
