810ea00fbe44790b18404405f58959653ad74781
yoyogold-a11 <tmrfack@gmail.com>
2021-07-12 16:21:33 -0500

    task14, gets score average

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -2,6 +2,6 @@
 -- The result column name should be average
 -- The database name will be passed as an argument of the mysql command
 SELECT
-    AVG(score)
+    AVG(score) AS average
 FROM 
     second_table;
