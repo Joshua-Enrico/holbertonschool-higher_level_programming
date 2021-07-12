88fa48fdc10ada457fc129e80652fe06db543542
yoyogold-a11 <tmrfack@gmail.com>
2021-07-12 18:11:22 -0500

    task101 display avr temperature in descending order

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -2,10 +2,10 @@
 -- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 SELECT
     city,
-    AVG(value) AS average_temp
+    AVG(value) AS avg_temp
 FROM 
     temperatures
 GROUP BY
     city
 ORDER BY
-    average_temp DESC;
+    avg_temp DESC;
