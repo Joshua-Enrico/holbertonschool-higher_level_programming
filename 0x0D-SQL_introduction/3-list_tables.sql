d27a402404f0adbed853f928a9b4b8e37ae4e01c
yoyogold-a11 <tmrfack@gmail.com>
2021-07-12 13:39:48 -0500

    tak3, lists all tables from a databse, database name will be passed as an argumen

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- script that lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command (in the following example: mysql is the name of the database)
+SHOW TABLES;
