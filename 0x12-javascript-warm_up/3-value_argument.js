cccbe51ed0afd53b62156ceace099e1b7d790d3c
yoyogold-a11 <tmrfack@gmail.com>
2021-08-02 03:40:08 -0500

    making exc

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 
-if (myArgs[0] == null) {
+if (myArgs[0] === undefined) {
   console.log('No argument');
 } else {
   console.log(myArgs[0]);
