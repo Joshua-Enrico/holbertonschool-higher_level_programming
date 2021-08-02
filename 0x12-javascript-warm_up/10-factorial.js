431cf469b6cd58b5d0bd52c31b7e72e39b2ffbad
yoyogold-a11 <tmrfack@gmail.com>
2021-08-02 13:04:48 -0500

    fixing code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
-
-if (isNaN(myArgs[0])) {
-  console.log(1);
-} else {
-  let factorial = 1;
-  for (let i = 1; i <= myArgs; i++) {
-    factorial *= i;
+function factorial (myArgs) {
+  if (isNaN(myArgs[0])) {
+    console.log(1);
+  } else {
+    let factorial = 1;
+    for (let i = 1; i <= myArgs; i++) {
+      factorial *= i;
+    }
+    return (factorial);
   }
-  console.log(factorial);
 }
+console.log(factorial(myArgs));
