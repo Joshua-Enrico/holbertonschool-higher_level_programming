4ed3323dc84273064292d5868057ae25bbf1d935
yoyogold-a11 <tmrfack@gmail.com>
2021-08-02 13:27:11 -0500

    fixed code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
+function compare (a, b) {
+  return a - b;
+}
 
 if (myArgs.length === 0 || myArgs.length < 2) {
   console.log(0);
@@ -11,6 +14,6 @@ if (myArgs.length === 0 || myArgs.length < 2) {
       list.push(parseInt(myArgs[arg]));
     }
   }
-  list = list.sort();
+  list = list.sort(compare);
   console.log(list[list.slice(2).length]);
 }
