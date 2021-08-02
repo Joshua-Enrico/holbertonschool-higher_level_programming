a2f5f337f0ce4cefecfdc992f6fe8aa47d155923
yoyogold-a11 <tmrfack@gmail.com>
2021-08-02 13:17:34 -0500

    testing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 const myArgs = process.argv.slice(2);
 function factorial (myArgs) {
   if (isNaN(myArgs[0])) {
-    console.log(1);
+    return (1);
   } else {
     let factorial = 1;
     for (let i = 1; i <= myArgs; i++) {
@@ -11,4 +11,4 @@ function factorial (myArgs) {
     return (factorial);
   }
 }
-console.log(factorial(parseInt(myArgs)));
+console.log(factorial(myArgs));
