fd349c0011cfdff853fcbf6560a49a36ee96152f
yoyogold-a11 <tmrfack@gmail.com>
2021-08-02 12:38:42 -0500

    testing code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 
-if (myArgs.length === 0 || myArgs.length < 2) {
+if (myArgs.length < 2) {
   console.log(0);
 } else {
   let list = [];
 
   for (const arg in myArgs) {
-    list.push(parseInt(myArgs[arg]));
+    if (!isNaN(myArgs[arg])) {
+      list.push(parseInt(myArgs[arg]));
+    }
   }
   list = list.sort();
-  console.log(list[list.length - 2]);
+  console.log(list[list.slice(2).length]);
 }
