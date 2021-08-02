f9e552daedce4b8ab66190abafc257a8215758cb
yoyogold-a11 <tmrfack@gmail.com>
2021-08-02 12:47:55 -0500

    fixing code

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,14 +2,7 @@
 const myArgs = process.argv.slice(2);
 let Sbiggest = 0;
 if (myArgs.length > 1) {
-  let list = [];
-
-  for (const arg in myArgs) {
-    if (!isNaN(myArgs[arg])) {
-      list.push(parseInt(myArgs[arg]));
-    }
-  }
-  list = list.sort();
-  Sbiggest = (list[list.slice(2).length]);
+  myArgs.sort();
+  Sbiggest = (myArgs[myArgs.slice(2).length]);
 }
 console.log(Sbiggest);
