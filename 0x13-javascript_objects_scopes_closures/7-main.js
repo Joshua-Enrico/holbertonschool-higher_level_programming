b3bda9e9e118d708ba1a7e35e00593bca3007e60
yoyogold-a11 <tmrfack@gmail.com>
2021-08-03 07:38:14 -0500

    several adittions

--- 0x13-javascript_objects_scopes_closures/7-main.js
+++ 0x13-javascript_objects_scopes_closures/7-main.js
@@ -3,4 +3,4 @@ const nbOccurences = require('./7-occurrences').nbOccurences;
 
 console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
 console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["H", 12, "c", "H", "HolbeHrton", 8], "H"));
+console.log(nbOccurences(['H', 12, 'c', 'H', 'HolbeHrton', 8], 'H'));
