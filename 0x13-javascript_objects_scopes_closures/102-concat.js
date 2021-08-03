f13304555ff983eaa91cb9ca108fd0ce210de052
yoyogold-a11 <tmrfack@gmail.com>
2021-08-03 12:45:04 -0500

    fixing

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
+const filea = myArgs[0];
+const fileb = myArgs[1];
+const filec = myArgs[2];
 
 const fs = require('fs');
 
-fs.readFile(myArgs[0], 'utf-8', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  fs.readFile(myArgs[1], 'utf-8', function (err2, data2) {
-    if (err2) {
-      throw err2;
-    }
-    const str = data + '\n' + data2 + '\n';
-    fs.writeFile(myArgs[2], str, function () {});
-  });
-});
+const texta = fs.readFileSync(filea, 'utf-8');
+const textb = fs.readFileSync(fileb, 'utf-8');
+fs.writeFileSync(filec, texta + '\n' + textb + '\n');
