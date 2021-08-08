883baeb9816d2ab62893100221d857f80807914b
yoyogold-a11 <tmrfack@gmail.com>
2021-08-08 14:59:06 -0500

    fixing code

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """
-contains the class
+....
 """
-
-import sqlalchemy
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
 
 Base = declarative_base()
 
 
-class State(Base):
-    """Representation of a state"""
-    __tablename__ = 'states'
+class City (Base):
+    """ City class """
+
+    __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
