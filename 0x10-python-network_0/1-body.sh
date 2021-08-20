b689e8e604dcf1c33c337afbc8680ab7492e33c9
yoyogold-a11 <tmrfack@gmail.com>
2021-08-19 19:57:56 -0500

    testing

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+
+# Display only body of a 200 status code response
+# You have to use curl
+
+curl -sL "$1"
