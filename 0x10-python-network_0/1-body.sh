fe90b8ae45f5ea8989a868cab2edcd3052c6b1cc
yoyogold-a11 <tmrfack@gmail.com>
2021-08-19 20:17:52 -0500

    testing

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-
 # Display only body of a 200 status code response
 # You have to use curl
 
-if [ "$(curl -sLw "%{http_code}" "$1" -o /dev/null)" -eq "200" ]; then curl -sL "$1"; fi 
+curl -sL "$1"
