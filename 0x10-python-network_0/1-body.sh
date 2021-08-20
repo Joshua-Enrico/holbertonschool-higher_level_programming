17bd8476f85a6c8876255a95a52e4915b735ea00
yoyogold-a11 <tmrfack@gmail.com>
2021-08-19 20:58:52 -0500

    testing

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-# Display only body of a 200 status code response
-# You have to use curl
-
 curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Write a Bash script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
-# You have to use curl
-
 curl -sX DELETE "$1"
