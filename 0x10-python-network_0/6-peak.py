30f9da811d44aee9667655b5fa6c44443b8c3ad8
yoyogold-a11 <tmrfack@gmail.com>
2021-08-19 22:22:27 -0500

    fixing format

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ Test function find_peak """
+
+
 def find_peak(list_of_integers):
     """finds a peak"""
+
     new_l = list_of_integers
-    l = len(new_l)
-    if l == 0:
+    le = len(new_l)
+    if le == 0:
         return
-    m = l // 2
-    if (m == l - 1 or new_l[m] >= new_l[m + 1]) and (m == 0 or new_l[m] >= new_l[m - 1]):
+    m = le // 2
+    if (m == l - 1 or new_l[m] >= new_l[m + 1]) and (m == 0 or new_l[m]
+                                                     >= new_l[m - 1]):
         return new_l[m]
-    if m != l - 1 and new_l[m + 1] > new_l[m]:
+    if m != le - 1 and new_l[m + 1] > new_l[m]:
         return find_peak(new_l[m + 1:])
     return find_peak(new_l[:m])
